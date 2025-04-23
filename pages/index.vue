8be99f37b72e1083d68efaf6f02672e47dd4aed4
naimnoh <amirul@talentcloud.ai>
2025-04-23 14:59:58 +0800

    [develop]: Update pages index: 1. Remove ImageViewer

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,21 @@
 <template>
-  <Swiper  style="height:100vh; width:100vw" direction="vertical" :slides-per-view="1" :slides-per-group="1" :loop="false" :mousewheel="{ sensitivity: 2 }" :keyboard="{
+  <Swiper
+    style="height: 100vh; width: 100vw"
+    direction="vertical"
+    :slides-per-view="1"
+    :slides-per-group="1"
+    :loop="false"
+    :mousewheel="{ sensitivity: 2 }"
+    :keyboard="{
       enabled: true,
-    }" :modules="[Mousewheel, Keyboard]" :effect="'smooth'" :speed="900"
-   >
+    }"
+    :modules="[Mousewheel, Keyboard]"
+    :effect="'smooth'"
+    :speed="900"
+  >
     <SwiperSlide>
       <DashSlide></DashSlide>
     </SwiperSlide>
-    <SwiperSlide>
-      <ImageViewer></ImageViewer>
-    </SwiperSlide>
-    
     <SwiperSlide>
       <Map></Map>
     </SwiperSlide>
@@ -17,14 +23,19 @@
 </template>
 
 <script setup>
-import 'swiper/css/autoplay';
-import 'swiper/css/effect-fade';
-import { EffectFade, Pagination, Navigation, Autoplay, Mousewheel, Keyboard } from 'swiper/modules';
-
+import "swiper/css/autoplay";
+import "swiper/css/effect-fade";
+import {
+  EffectFade,
+  Pagination,
+  Navigation,
+  Autoplay,
+  Mousewheel,
+  Keyboard,
+} from "swiper/modules";
 </script>
 
 <style scoped>
-
 .swiper-button-prev {
   color: white !important;
 }
